457b24d73a0dae1c6b5a6f35cf426a91547e00c5
dumiao <1035396480@qq.com>
2016-05-15 17:21:22 +0800

    生成word

--- generate_word.py
+++ generate_word.py
@@ -32,7 +32,7 @@ def convert_pdf1(txtpath,type):
 
   #subprocess.popen("unoconv -f  pdf tem.docx")
   os.popen("rm tem.docx")
-def convert_pdf2(title,content,path,filetype):
+def save_word(title,content,path,filetype):
   print title
   document = Document("template/"+filetype+".docx")
   cursor_paragraph=document.paragraphs[-1]
@@ -47,16 +47,12 @@ def convert_pdf2(title,content,path,filetype):
   qq_style=styles.add_style('qq', WD_STYLE_TYPE.PARAGRAPH)
   qq_style.font.color.rgb=RGBColor(200,10,10)
   qq_style.font.size=Pt(15)
-  qq_txt=cursor_paragraph.insert_paragraph_before(u"\n\n"+qq[filetype],style="qq")
-  qq_txt.alignment=2
   #qq_txt.style.font.color=
   tem_docname=path+'/'+filetype+"/"+title+".docx"
-  print "呵呵"
-  print tem_docname
   document.save(tem_docname)
-  print "lowriter --convert-to pdf "+title+".docx"
+  #print "lowriter --convert-to pdf "+title+".docx"
   #subprocess.call(["lowriter","--convert-to","pdf",tem_docname])
   #os.popen("lowriter --convert-to pdf "+title+".docx")
   #os.popen("rm tem.docx")
 if __name__=="__main__":
-  convert_pdf2(u"托福口语&写作备考：模版是把双刃剑".encode("utf-8"),"sdfsdf","pdf/2016-03-20/","GRE")
+  save_word(u"托福口语&写作备考：模版是把双刃剑".encode("utf-8"),"sdfsdf","pdf/2016-05-09/","GRE")
